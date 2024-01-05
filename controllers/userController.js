944ecf99cd5479269b03487e44e6f2a018537f10
danyon talbot <danyon.talbot2@gmail.com>
2024-01-05 19:11:01 +1100

    Add Friends Functionality Added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const mongoose = require('mongoose');
 
 
 module.exports = {
@@ -67,4 +68,56 @@ module.exports = {
             res.status(500).json({ error: 'Server error' });
         }
     },
+
+    async addFriend(req, res) {
+        try {
+          // Capture userId and friendId from route parameters
+          const userId = req.params.userId;
+          const friendId = req.params.friendId;
+      
+          // Check if userId and friendId are valid MongoDB ObjectIDs
+          if (!mongoose.Types.ObjectId.isValid(userId) || !mongoose.Types.ObjectId.isValid(friendId)) {
+            return res.status(400).json({ error: 'Invalid ObjectIDs' });
+          }
+      
+          // Find the user with userId and add friendId to their friends array
+          const user = await User.findOneAndUpdate(
+            { _id: userId }, // No need to cast as ObjectId here
+            { $addToSet: { friends: friendId } }, // No need to cast as ObjectId here
+            { new: true }
+          );
+      
+          // Handle the case where the user with userId is not found
+          if (!user) {
+            return res.status(404).json({ message: `User with ID ${userId} not found` });
+          }
+      
+          // Send a success response
+          res.json({ message: 'Friend added!' });
+        } catch (err) {
+          // Handle server errors gracefully
+          res.status(500).json({ error: 'Server error' });
+        }
+      },
+      
+    
+      // DELETE a friend from a User's friend list
+      async removeFriend(req, res) {
+        try {
+          const user = await User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { new: true }
+          );
+    
+          if (!user) {
+            return res.status(404).json({ message: 'No user with that ID' });
+          }
+    
+          res.json({ message: 'Friend removed!' });
+        } catch (err) {
+          res.status(500).json({ error: 'Server error' });
+        }
+      },
+    
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,23 @@ const {
     getUsers,
     getSingleUser,
     createUser,
-    deleteUser
+    deleteUser,
+    addFriend,
+    removeFriend
 } = require('../../controllers/userController');
+
+// const {
+//     addFriend,
+//     removeFriend
+// } = require('../../controllers/userFriendController')
 
 console.log('User routes initialised');
 
 router.route('/').get(getUsers).post(createUser);
 
 router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
 
 module.exports = router;
