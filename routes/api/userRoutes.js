151c6eff71ac28fc6d1e79b7245c2237aa943fa1
danyon talbot <danyon.talbot2@gmail.com>
2024-01-05 20:59:27 +1100

    Added PUT API Route for User

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,6 +5,7 @@ const {
     getSingleUser,
     createUser,
     deleteUser,
+    updateUser,
     addFriend,
     removeFriend
 } = require('../../controllers/userController');
@@ -18,7 +19,7 @@ console.log('User routes initialised');
 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:userId').get(getSingleUser).delete(deleteUser);
+router.route('/:userId').get(getSingleUser).delete(deleteUser).put(updateUser);
 
 router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
 
